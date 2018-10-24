bb4a47f71a3b152a4b07299018eb1a9df976978a
Chris Navrides <chris@test.ai>
2018-10-23 17:31:00 -0700

    initial commit

---
+++ sample_run/run_model.py
@@ -0,0 +1,111 @@
+import argparse
+import os
+
+import numpy as np
+import tensorflow as tf
+
+
+def load_graph(model_file):
+  graph = tf.Graph()
+  graph_def = tf.GraphDef()
+
+  with open(model_file, "rb") as f:
+    graph_def.ParseFromString(f.read())
+  with graph.as_default():
+    tf.import_graph_def(graph_def)
+
+  return graph
+
+
+def read_tensor_from_image_file(file_name,
+                                input_height=299,
+                                input_width=299,
+                                input_mean=0,
+                                input_std=255):
+  input_name = "file_reader"
+  output_name = "normalized"
+  file_reader = tf.read_file(file_name, input_name)
+  if file_name.endswith(".png"):
+    image_reader = tf.image.decode_png(
+        file_reader, channels=3, name="png_reader")
+  elif file_name.endswith(".gif"):
+    image_reader = tf.squeeze(
+        tf.image.decode_gif(file_reader, name="gif_reader"))
+  elif file_name.endswith(".bmp"):
+    image_reader = tf.image.decode_bmp(file_reader, name="bmp_reader")
+  else:
+    image_reader = tf.image.decode_jpeg(
+        file_reader, channels=3, name="jpeg_reader")
+  image_reader = tf.image.grayscale_to_rgb(tf.image.rgb_to_grayscale(image_reader)) # Convert to grayscale
+  float_caster = tf.cast(image_reader, tf.float32)
+  dims_expander = tf.expand_dims(float_caster, 0)
+  resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
+  normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
+  sess = tf.Session()
+  result = sess.run(normalized)
+
+  return result
+
+
+def load_labels(label_file):
+  label = []
+  proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
+  for l in proto_as_ascii_lines:
+    label.append(l.rstrip())
+  return label
+
+
+if __name__ == "__main__":
+  current_dir = os.path.dirname(os.path.realpath(__file__))
+  classifier_data_dir = os.path.join(current_dir, 'testai_model')
+
+  file_name = None
+  model_file = os.path.join(classifier_data_dir, 'saved_model.pb')
+  label_file = os.path.join(classifier_data_dir, 'saved_model.pbtxt')
+  input_height = 224
+  input_width = 224
+  input_mean = 0
+  input_std = 255
+  threshold = 0.3
+  input_layer = "Placeholder"
+  output_layer = "final_result"
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--image", help="image to be processed")
+  args = parser.parse_args()
+
+  if args.image:
+    file_name = args.image
+
+  if file_name is None:
+    print ('ERROR! Need image to run prediction on. Please add --image <path_to_file>')
+    exit(1)
+
+  graph = load_graph(model_file)
+  t = read_tensor_from_image_file(
+      file_name,
+      input_height=input_height,
+      input_width=input_width,
+      input_mean=input_mean,
+      input_std=input_std)
+
+  input_name = "import/%s" % input_layer
+  output_name = "import/%s" % output_layer
+  input_operation = graph.get_operation_by_name(input_name)
+  output_operation = graph.get_operation_by_name(output_name)
+
+  with tf.Session(graph=graph) as sess:
+    results = sess.run(output_operation.outputs[0], {
+        input_operation.outputs[0]: t
+    })
+  results = np.squeeze(results)
+
+  top_k = results.argsort()[-5:][::-1]
+  labels = load_labels(label_file)
+
+  # Output the results
+  print('Top results:')
+  for i in top_k:
+    if results[i] < threshold:
+        break
+    print('    %s - %f' % (labels[i], results[i]))
